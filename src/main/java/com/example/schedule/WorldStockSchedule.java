25734ac0af279a8a3d9e659cf9ad85c6487faa03
yongjung95 <yongjung95@gmail.com>
2024-06-05 20:30:18 +0900

    :white_check_mark: 미국 주식 정보 스케줄링 추가

---
+++ src/main/java/com/example/schedule/WorldStockSchedule.java
@@ -0,0 +1,48 @@
+package com.example.schedule;
+
+import com.example.service.DartApiService;
+import com.example.service.WorldStockService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+@Slf4j
+public class WorldStockSchedule {
+
+    private boolean isInitialRun = true;
+
+    private final WorldStockService service;
+
+    @Autowired
+    public WorldStockSchedule(WorldStockService service) {
+        // 빈 초기화 시 첫 실행 설정
+        this.service = service;
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        // 서버 시작 시 한 번 실행
+        reportCurrentTime();
+        isInitialRun = false;
+    }
+
+    @Scheduled(cron = "0 0 1 ? * TUE")
+    public void reportCurrentTime() {
+        if (isInitialRun) {
+            // 서버 시작 시 한 번 실행한 이후 주기적으로 실행
+            service.worldStockInfoUpdate("AMEX");
+            service.worldStockInfoUpdate("NASDAQ");
+            service.worldStockInfoUpdate("NYSE");
+        } else {
+            service.worldStockInfoUpdate("AMEX");
+            service.worldStockInfoUpdate("NASDAQ");
+            service.worldStockInfoUpdate("NYSE");
+        }
+    }
+}
--- src/test/java/com/example/service/WorldStockServiceTest.java
+++ src/test/java/com/example/service/WorldStockServiceTest.java
@@ -1,11 +1,14 @@
 package com.example.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 @SpringBootTest
 class WorldStockServiceTest {
 
@@ -14,6 +17,23 @@ class WorldStockServiceTest {
 
     @Test
     void 해외주식_종목호출테스트() {
-        worldStockService.getStockInfo("AMEX");
+        worldStockService.worldStockInfoUpdate("AMEX");
+    }
+
+
+    @Test
+    void NOTIFY_토큰정보_가져오기() {
+        String token = worldStockService.getToken();
+
+        log.info("token = " + token);
+    }
+
+    @Test
+    void 해외주식_한글상품명_가져오기() {
+        String koreanName = worldStockService.getStockKoreanName("AAPL", "NASDAQ");
+
+        Assertions.assertThat(koreanName).isEqualTo("애플");
+        log.info("koreanName = " + koreanName);
+
     }
 }
